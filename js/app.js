34e377a64d161a78dbc3ae18231baef4ccdf3922
XIIFulminata <jovaunjackson@gmail.com>
2017-03-19 00:30:01 -0400

    add skill icons + test hiscores_lite api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-$(document).foundation()
+
+$(document).ready(function() {
+    var playerData;
+    
+    $("#go-btn").click(function() {
+        getPlayerData("xiifulminata").done(function(data) {
+        playerData = formatPlayerData(data);
+        var str = $("#player-attack-level").text('99');
+        console.log(str);
+      })
+  
+});
+});
+
+
+function getPlayerData(username) {
+    var url = "http://services.runescape.com/m=hiscore/index_lite.ws?player=" + username;
+    
+    return $.get(url,function(data) {
+        
+    });
+   
+}
+
+
+function formatPlayerData(d) {
+    var formatted = d.split("\n");
+    console.log(formatted);
+}
+
