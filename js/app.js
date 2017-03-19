1bdbbfcf4cc9f7b3f740754617a40717a3fc6621
XIIFulminata <jovaunjackson@gmail.com>
2017-03-19 04:09:55 -0400

    can update levels with my username

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
     $("#go-btn").click(function() {
         getPlayerData("xiifulminata").done(function(data) {
         playerData = formatPlayerData(data);
-        var str = $("#player-attack-level").text('99');
-        console.log(str);
+        //var str = $("#player-attack-level").text('99');
+        //console.log(str);
       })
   
 });
@@ -25,6 +25,14 @@ function getPlayerData(username) {
 
 function formatPlayerData(d) {
     var formatted = d.split("\n");
+    formatted=formatted.slice(0,28);
+    // index 0 is the rank - 1 is the level - 2 is the xp
+    var total = formatted[0].split(",")[1];
+    var attack = formatted[1].split(",")[1];
+    attack = $("#player-attack-level").text(attack);
+    var woodcutting = formatted[9].split(",")[1];
+    woodcutting = $("#player-woodcutting-level").text(woodcutting);
+    console.log(total);
     console.log(formatted);
 }
 
